adae93f340a38808b5baab9cc214c4e6fdfd2fc6
Ankit Chahal <ankit.chahal1@ibm.com>
2022-02-23 01:57:53 +0530

    Adding comments

--- server.go
+++ server.go
@@ -42,6 +42,8 @@ func main() {
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
+
+	//log.Fatal(http.ListenAndServe(":8080", r)) Without any interrupt
 	// Start Server
 	go func() {
 		log.Println("Starting Server")
@@ -54,7 +56,24 @@ func main() {
 }
 
 func waitForShutdown(srv *http.Server) {
+
+	/*
+
+		if somewhere you are listening for output from a channel but in the meanwhile that channel not being given any input,
+		the listening place will be blocked until it receives the output.
+
+	*/
+
 	interruptChan := make(chan os.Signal, 1)
+
+	/*
+
+		SIGINT means is Signal Interrupted, send when the user types the INTR character (e.g. Ctrl-C).
+		SIGTERM signal is a generic signal used to cause program termination.
+		The below statement is to send the interruot signal to the channel "interruptChan".
+
+	*/
+
 	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	// Block until we receive our signal.
@@ -62,7 +81,24 @@ func waitForShutdown(srv *http.Server) {
 
 	// Create a deadline to wait for.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+
+	/*
+
+		some zombie connections may still be there and use your memory,
+		in order to solve this, the safest way is to setup a timeout threshold using the Context.
+
+	*/
+
 	defer cancel()
+
+	/*
+
+		The timer context and the cancel function (the cancel function releases its resources) will be returned,
+		 and you can use that context to perform Shutdown(ctx) ,
+		inside the Shutdown it check if the timer context Done channel is closed and will not run indefinitely.
+
+	*/
+
 	srv.Shutdown(ctx)
 
 	log.Println("Shutting down")
